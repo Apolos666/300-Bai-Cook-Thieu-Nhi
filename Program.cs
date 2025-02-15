eb3b916b2a77ebf1780a778ab5bdaf45fc3a7c45
Apolos666 <quangprox121@gmail.com>
2025-02-15 16:14:19 +0700

    Bai 57 Hello Interview solution

--- Program.cs
+++ Program.cs
@@ -1,12 +1,38 @@
-﻿int[] nums1 = [1, 2, 2, 1];
-int[] nums2 = [2, 2];
+﻿var intervals = new int[][] {
+    [1,2],[3,5],[6,7],[8,10],[12,16]
+};
 
-Console.Write(string.Join(" ", Intersection(nums1, nums2)));
+var newInterval = new int[] {4,8};
 
-int[] Intersection(int[] nums1, int[] nums2)
+var result = Insert(intervals, newInterval);
+
+foreach (var item in result)
 {
-    var hashset1 = new HashSet<int>(nums1);
-    var hashset2 = new HashSet<int>(nums2);
+    Console.WriteLine($"[{item[0]},{item[1]}]");
+}
 
-    return hashset1.Intersect(hashset2).ToArray();
+int[][] Insert(int[][] intervals, int[] newInterval)
+{
+    var result = new List<int[]>();
+        
+    var i = 0;
+    while (i < intervals.Length && intervals[i][1] < newInterval[0]) {
+        result.Add(intervals[i]);
+        i++;
+    } 
+        
+    while (i < intervals.Length && intervals[i][0] <= newInterval[1]) {
+        newInterval[0] = Math.Min(newInterval[0], intervals[i][0]);
+        newInterval[1] = Math.Max(newInterval[1], intervals[i][1]);
+        i++;
+    }
+        
+    result.Add(newInterval);
+        
+    while (i < intervals.Length) {
+        result.Add(intervals[i]);
+        i++;
+    }
+        
+    return result.ToArray();
 }
